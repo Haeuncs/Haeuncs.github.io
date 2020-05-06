7fecba947a8eb419e9870e5828cc11702fd461f5
Haeuncs <haeun.developer@gmail.com>
2020-05-06 20:05:32 +0900

    add post

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-blog-post-js": hot(preferDefault(require("/Users/leehaeun/Haeuncs.github.io__/src/templates/blog-post.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/leehaeun/Haeuncs.github.io__/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/leehaeun/Haeuncs.github.io__/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/leehaeun/Haeuncs.github.io__/src/pages/index.js"))),
   "component---src-pages-portfolio-2-js": hot(preferDefault(require("/Users/leehaeun/Haeuncs.github.io__/src/pages/portfolio_2.js"))),
