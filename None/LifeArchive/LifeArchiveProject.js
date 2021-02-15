95de55681fdf660f294f2ecc9286916915e84844
Haeuncs <haeun.developer@gmail.com>
2021-02-15 23:57:07 +0900

    archive

--- None/LifeArchive/LifeArchiveProject.js
+++ None/LifeArchive/LifeArchiveProject.js
@@ -1,30 +1,30 @@
-// import React from "react";
-// import "../../css/global.css";
-// import LifeImageTable from "../components/LifeArchive/LifeImageTable";
+import React from "react";
+import "../../css/global.css";
+import LifeImageTable from "../components/LifeArchive/LifeImageTable";
 
-// const LifeArchiveProject = ({ data, location }) => {
-//   return (
-//     <div
-//       className="flex flex-col text-28 font-900 text-white"
-//       style={{ backgroundColor: "#000", height: "100vh", margin: 0 }}
-//     >
-//       <div>
-//       </div>
-//       <div className="flex-1" style={{ height: "100%" }}>
-//         <LifeImageTable></LifeImageTable>
-//       </div>
-//     </div>
-//   );
-// };
+const LifeArchiveProject = ({ data, location }) => {
+  return (
+    <div
+      className="flex flex-col text-28 font-900 text-white"
+      style={{ backgroundColor: "#000", height: "100vh", margin: 0 }}
+    >
+      <div>
+      </div>
+      <div className="flex-1" style={{ height: "100%" }}>
+        <LifeImageTable></LifeImageTable>
+      </div>
+    </div>
+  );
+};
 
-// export default LifeArchiveProject;
+export default LifeArchiveProject;
 
-// export const pageQuery = graphql`
-//   query {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//   }
-// `;
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
